7ec81394316866b9e8ec38ca03b16887d8d92694
ejrbuss <ebuss@ualberta.ca>
2017-04-17 03:53:09 -0700

    updated .codeclimate.yml

--- src/extensions/implements.js
+++ src/extensions/implements.js
@@ -2,17 +2,17 @@ var type = require('../type-mark');
 require('./types');
 require('./object');
 
-function _implements(interface, arg) {
-    if(type(interface).function) {
-        return interface.apply(this, [arg]);
+function _implements(_interface, arg) {
+    if(type(_interface).function) {
+        return _interface.apply(this, [arg]);
     }
-    type(interface).assert.object;
-    return Object.keys(interface).every(function(key) {
-        return type(arg).object && _implements(interface[key], arg[key]);
+    type(_interface).assert.object;
+    return Object.keys(_interface).every(function(key) {
+        return type(arg).object && _implements(_interface[key], arg[key]);
     });
 }
 
 // Extension code
-type.extendfn('implements', _implements, function(interface, arg) {
+type.extendfn('implements', _implements, function(_interface, arg) {
     return arg + ' fails to implement interface';
 });
